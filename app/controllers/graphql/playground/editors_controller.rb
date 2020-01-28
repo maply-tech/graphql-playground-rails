b12751478ed9d0acbaae726d6c0291ae28e7c4fa
Henrique Sobral Gomes <hsobral@gmail.com>
2020-01-28 14:23:20 -0300

    Update editors_controller.rb

--- app/controllers/graphql/playground/editors_controller.rb
+++ app/controllers/graphql/playground/editors_controller.rb
@@ -2,6 +2,9 @@ module GraphQL
   module Playground
     class EditorsController < ApplicationController
       def show
+        Rails.logger.info params.inspect
+        Rails.logger.info root_url
+        Rails.logger.info request.host_with_port
       end
     end
   end
